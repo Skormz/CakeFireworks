c0da1eddaf0d3e7fc71f1d36294bad74d1df86ca
Hunter Watt <Skorm@ymail.com>
2020-02-10 09:17:18 -0800

    First test of camera scroll

---
+++ Assets/Scripts/CameraScroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour
+{
+    public float autoScrollSpeed;
+    public float startTimer = 3f;
+
+    private GameObject cameraPosition;
+    private bool gameStart;
+    
+    
+    
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        startTimer = startTimer -= Time.deltaTime;
+
+        if (startTimer <= 0)
+        {
+            gameStart = true;
+        }
+
+        if (gameStart == true)
+        {
+            transform.Translate(autoScrollSpeed, 0f, 0f);
+        }
+        
+    }
+
+}
