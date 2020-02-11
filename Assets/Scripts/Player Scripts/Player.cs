8ebe26123e4a297dcc4bb2925252d806f3a9500f
Beaumerang <48000800+Beaumerang@users.noreply.github.com>
2020-02-10 18:23:52 -0800

    Player animation fully working

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -7,10 +7,12 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask detectPlatforms;
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxCollider2d;
+    public Animator animator;
 
     public float jumpPower = 10f;
     public float movementSpeed = 40f;
     public float autoMovementSpeed = 0f;
+    bool facingRight = true;
 
     private void Awake()
     {
@@ -23,7 +25,14 @@ public class Player : MonoBehaviour
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
             rigidbody2d.velocity = Vector2.up * jumpPower;
+            animator.SetTrigger("Jump");
         }
+
+        if (IsGrounded() == false)
+            animator.SetBool("Airborn",true);
+        else
+            animator.SetBool("Airborn",false);
+
 
         HandleMovement();
     }
@@ -40,17 +49,32 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rigidbody2d.velocity = new Vector2(-movementSpeed, rigidbody2d.velocity.y);
+            animator.SetFloat("Speed", Mathf.Abs(movementSpeed));
+            if (facingRight)
+                Flip();
         }
         else
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 rigidbody2d.velocity = new Vector2(+movementSpeed, rigidbody2d.velocity.y);
+                animator.SetFloat("Speed", Mathf.Abs(movementSpeed));
+                if (!facingRight)
+                    Flip();
             }
             else
             {
                 rigidbody2d.velocity = new Vector2(autoMovementSpeed, rigidbody2d.velocity.y);
+                animator.SetFloat("Speed", 0f);
             }
         }
+    }
+
+    public void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
