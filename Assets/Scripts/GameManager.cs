6a238f1750d69079470ac5e0dd2f366cb9b0d6d4
Beaumerang <48000800+Beaumerang@users.noreply.github.com>
2020-02-12 11:46:50 -0800

    CAKE IS WORKING!
    no visual yet but the score is being kept by the game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddPoints(int amount)
+    {
+        score += amount;
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPoints : MonoBehaviour
+{
+    public GameManager manager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals ("Cake"))
+        {
+            manager.SendMessage("AddPoints",collision.gameObject.GetComponent<Cake>().cakeValue);
+            Destroy(collision.gameObject);
+        }
+    }
+}
