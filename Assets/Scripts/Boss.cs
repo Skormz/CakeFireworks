5522ded3ffa43c12c8e0592934e3eb2267e509a3
Beaumerang <48000800+Beaumerang@users.noreply.github.com>
2020-02-13 10:48:03 -0800

    bossman

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+            transform.Translate (Vector2.right * speed * Time.deltaTime);
+    }
+
+}
